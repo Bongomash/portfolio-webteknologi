4e8b0fe745b0a5dabcc580c9087334db61c73bb0
Marc H <marcdahlhendriksen@gmail.com>
2024-10-13 17:35:34 +0200

    last minuted changes on som html and js

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 
 
-// Get references to the button and the body
+// Declare doms
 const modeToggleButton = document.getElementById('modeToggle');
 const body = document.body;
 
 modeToggleButton.addEventListener('click', () => {
-    // Toggle the 'light-mode' class on the body
+
     body.classList.toggle('light-mode');
 
     // Change the button text based on the current mode
     if (body.classList.contains('light-mode')) {
-        modeToggleButton.textContent = 'Switch to Dark Mode'; // Change button text
+        modeToggleButton.textContent = 'Switch to Dark Mode';
     } else {
-        modeToggleButton.textContent = 'Switch to Light Mode'; // Change button text
+        modeToggleButton.textContent = 'Switch to Light Mode';
     }
 });
